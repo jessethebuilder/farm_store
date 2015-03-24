9d99f0c19d119e03bdfd7bf76803b84fb4a6baf9
Jesse Farmer <jessethebuilder@yahoo.com>
2015-03-23 21:50:42 -0700

    FarmStoreProfile

--- app/models/farm_store_profile.rb
+++ app/models/farm_store_profile.rb
@@ -1,2 +1,5 @@
 class FarmStoreProfile < ActiveRecord::Base
+  belongs_to :has_farm_store_profile, polymorphic: true
+
+  validates :standard_tax_rate, :presence => true, :numericality => {:greater_than_or_equal_to => 0}
 end
