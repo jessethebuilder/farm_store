99ca016934a8bf985e4f37c56deab1085f6f3d55
Jesse Farmer <jessethebuilder@yahoo.com>
2015-04-02 17:33:14 -0700

    FarmStoreProfile

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,62 +1,60 @@
-require_dependency "store/application_controller"
-
-module FarmStore
-  class OrdersController < ApplicationController
-    before_action :set_order, only: [:show, :edit, :update, :destroy]
-
-    # GET /orders
-    def index
-      @orders = Order.all
-    end
-
-    # GET /orders/1
-    def show
-    end
-
-    # GET /orders/new
-    def new
-      @order = Order.new
-    end
-
-    # GET /orders/1/edit
-    def edit
-    end
-
-    # POST /orders
-    def create
-      @order = Order.new(order_params)
-
-      if @order.save
-        redirect_to @order, notice: 'Order was successfully created.'
-      else
-        render :new
-      end
-    end
-
-    # PATCH/PUT /orders/1
-    def update
-      if @order.update(order_params)
-        redirect_to @order, notice: 'Order was successfully updated.'
-      else
-        render :edit
-      end
-    end
-
-    # DELETE /orders/1
-    def destroy
-      @order.destroy
-      redirect_to orders_url, notice: 'Order was successfully destroyed.'
-    end
-
-    private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_order
-        @order = Order.find(params[:id])
-      end
-
-      # Only allow a trusted parameter "white list" through.
-      def order_params
-        params.require(:order).permit(:items, :has_orders_id, :has_orders_type, :phase, :billing_address, :mailing_address)
-      end
-  end
-end
+# module FarmStore
+#   class OrdersController < ApplicationController
+#     before_action :set_order, only: [:show, :edit, :update, :destroy]
+#
+#     # GET /orders
+#     def index
+#       @orders = Order.all
+#     end
+#
+#     # GET /orders/1
+#     def show
+#     end
+#
+#     # GET /orders/new
+#     def new
+#       @order = Order.new
+#     end
+#
+#     # GET /orders/1/edit
+#     def edit
+#     end
+#
+#     # POST /orders
+#     def create
+#       @order = Order.new(order_params)
+#
+#       if @order.save
+#         redirect_to @order, notice: 'Order was successfully created.'
+#       else
+#         render :new
+#       end
+#     end
+#
+#     # PATCH/PUT /orders/1
+#     def update
+#       if @order.update(order_params)
+#         redirect_to @order, notice: 'Order was successfully updated.'
+#       else
+#         render :edit
+#       end
+#     end
+#
+#     # DELETE /orders/1
+#     def destroy
+#       @order.destroy
+#       redirect_to orders_url, notice: 'Order was successfully destroyed.'
+#     end
+#
+#     private
+#       # Use callbacks to share common setup or constraints between actions.
+#       def set_order
+#         @order = Order.find(params[:id])
+#       end
+#
+#       # Only allow a trusted parameter "white list" through.
+#       def order_params
+#         params.require(:order).permit(:items, :has_orders_id, :has_orders_type, :phase, :billing_address, :mailing_address)
+#       end
+#   end
+# end
